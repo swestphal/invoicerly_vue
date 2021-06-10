af94dd8c900d7c255e73251790ac74c508dde934
Simone Westphal <hello@swestphal.io>
2021-06-10 14:49:44 +0200

    conf: Configure store and initial setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,33 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+import router from "../router";
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        user: {
+            username: ''
+        },
+        isAuthenticated: false,
+        token: ''
+    },
+    mutations: {
+        initializeStore(state) {
+            if (localStorage.getItem('token')) {
+                state.token = localStorage.getItem('token')
+                state.isAuthenticated = true
+            } else {
+                state.token = ''
+                state.isAuthenticated = false
+            }
+        },
+        setToken(state,token) {
+            state.token = token
+            state.isAuthenticated = true
+        },
+        removeToken(state) {
+            state.token=''
+            state.isAuthenticated=false
+        }
+    },
+    actions: {},
+    modules: {}
 })
